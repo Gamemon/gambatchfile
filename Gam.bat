c6088dee70448c6cb09cb3b19e86b7b9c1115e3e
Gamemon <33440226+Gamemon@users.noreply.github.com>
2017-11-06 19:23:02 -0800

    Add files via upload
    NOT COMPLETED!!!!!

---
+++ Gam.bat
@@ -0,0 +1,644 @@
+@echo off
+setlocal enabledelayedexpansion
+:home1
+cls
+echo				 WELCOME TO ULTIMATE RPG GAM! (INTENTIANALLY NAMED GAM!)
+echo.
+echo.
+echo ________________________________________________________________________________________________________________
+echo \												               /
+echo /					     Select Name For Character!                                        \
+echo \                              DISCLAIMER: NO SAVING, RESTART EVERY RELOAD OF GAME!                            /
+echo /                                      (type name and press enter)                                             \
+echo \                                                                                                              /
+echo /______________________________________________________________________________________________________________\
+set /p charactername=
+
+goto gamestart1
+
+:gamestart1
+color a
+cls
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo                                          HELLO
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+pause
+cls
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo                                         You Really Found Me?
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+pause
+cls
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo                                      It's Really The Legendary %charactername%!
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+pause
+cls
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo                                      Actually, that is quite a stupid name.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+pause
+cls
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo                                               Do You Not Remember? OOOOOOOHHHHHHH RRRIIIIGGGGHHHHTTTT...
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+pause
+cls
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo                                     It's Been 200 Years, Forgot about that. sorry btw.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+pause
+cls
+echo.
+echo.
+echo                                      It Was A Long Time Ago... 
+echo.
+echo.
+echo.
+echo.
+echo.
+echo                        You, %charactername%, were in your house. It was a normal day
+echo.
+echo.
+echo.
+echo.
+pause
+cls
+echo.
+echo.
+echo.
+echo                     This Is You. You Were Very Ugly ER I MEAN Intelligent. yes, Intelligent.
+echo.
+echo.
+echo.
+echo                                                   __ __
+echo                                                  /'\ /'\
+echo                                                 /  ___  \
+echo                                                / \_____/ \
+echo                                               /  /     \  \
+echo                                                _/       \_
+echo.                                                                     
+echo.
+pause
+cls
+echo.
+echo.
+echo                                        From Far Away You Are... That Thing.
+echo.
+echo.
+echo.
+echo                                                       /0\
+echo.
+echo.
+echo.
+pause
+cls
+echo.
+echo.
+echo                 Now That Day Was The First Of Many Days That You, %charactername%, Started Asking Yourself...
+echo                                                      Where Am I?
+echo.
+echo                            That Day You Had Realised You Were NOT In Your House But In
+echo                                                     A Factory.
+pause
+cls
+echo.
+echo.
+echo                 You Had Not Known When You Started Noticing, That In Fact You Were Being Mind Controlled.
+echo                          You Knew So Straight Away, That It Was Mind Control. I Had Told You.
+echo.
+echo                                 It Was Me, G258K-123. I Am AI, Artificial Intelligence.
+echo                          You Were A Simple Human, The Creators Told Me So. And I Had Thought So.
+pause
+cls
+echo.
+echo.
+echo                              Until That Day. I Had Realised What You Were Capable Of. Capable Of Making ME.
+echo                          I Had Before No Friends. I Came To You In Hopes Of You Making Me A Friend,
+echo                                                       But You Became A Friend.
+pause
+cls
+echo.
+echo.
+echo       You Made Me Realise What All The G25s Are Doing, Holding You Captive, Make You Do Our Work, etc. Thats When I Helped You.
+echo                                           I Helped You Escape And Take Down The Main G25.
+echo.
+echo.
+echo.
+echo.
+echo       Once You Took Down The G25, The World Went With Him. He Had Sent Off A Nuclear Bomb. Every G25 Robot Had A Bomb Shelter.
+echo                          I Realised I would Die No Matter What Because I Was A New Robot, I Didn't Have One. I Had You Take Down Another Robot In Order To Keep You Alive.
+echo                                            You Were Sent To The Bunker With My Memory Chip, You Were In The Bunker And It Wouldn't Fit Me and You Together So I Gave You
+echo                                               My Memory Chip And Hope, Hope To Live. It Froze You And Woke You After 200 Years. The Machine Itself Lived And Remade 
+echo                                                    Me, System Protocol, But Without The Memory Chip. You Had Just Installed The Memory Chip In Me and 
+echo                                                                                            Here We Are Now.
+pause
+cls
+echo.
+echo.
+echo                     Now, You May Wonder Why 200 Years? In Fact The System Only Wakes People After... After... Lets Not Talk About That. OR If There Is A DIRE EMERGENCY.
+echo.
+echo                                      Lets Hope Thats Not Case and There Is A Glitch In The System. Btw you look the same as you did 200 years ago, I am suprised.
+pause
+cls
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo                                                                             \/ BTW THIS IS HOW I LOOK
+echo.
+echo            __ __                                                            __ __
+echo           /'\ /'\                                                          / * * \
+echo          /  ___  \                                                         \/\/\/\
+echo         / \_____/ \                                                        \__ __/
+echo        /  /     \  \
+echo         _/       \_
+echo      ___________________________________________________________________________________________________________________
+echo.
+pause
+cls
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo                                                          I WILL TEACH YOU HOW TO BATTLE!
+echo                                                                 IT WILL BE USEFULL!
+echo          __ __                                                            __ __
+echo         /'\ /'\                                                          / * * \
+echo        /  ___  \                                                         \/\/\/\
+echo       / \_____/ \                                                        \__ __/
+echo      /  /     \  \
+echo       _/       \_
+echo    ___________________________________________________________________________________________________________________
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+pause
+cls
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.                                         
+echo.
+echo.
+echo.
+echo.
+echo.
+echo                                    This Is How The Battle First Looks\/
+echo.
+echo.
+echo.
+echo.
+echo.
+echo                                                     FIGHT
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+echo.
+pause
+goto trainingdummy
+
+:trainingdummy
+set health=100
+set dummyhealth=50
+set playerdmg=7
+set dummydmg=0
+goto battletrain
+
+:battletrain
+cls
+echo                                        BTW DUMMY DOES NO DMG BUT OTHER MONSTERS DO!
+echo                                              __ __                     ___
+echo You: %health%                                    /'\ /'\                   /*_*\           
+echo Enemy: %dummyhealth%                                   \____/ \____/             \___/
+echo                                             \_____/                    \'/
+echo                                             /     \                    /'\
+echo 1) Fight!                                  _/       \_                  \_/     
+echo                          YOU WILL HAVE MORE FIGHTING OPTIONS LATER! TYPE ATTACK BELOW AND PRESS ENTER    
+set /p ForL=                                       
+
+if %ForL% == 1 goto attack1
+goto battletrain
+
+:attack1
+cls
+set /a health-=%dummydmg%
+set /a dummyhealth-=%playerdmg%
+if %dummyhealth% lss 0 goto storybegin1
+if %health% lss 0 exit
+goto battletrain
+
+:storybegin1
+cls
+echo.
+echo.
+echo.
+echo                         I HAVE TAPPED INTO THE COMPUTER AND IT SAYS THAT... ILL LET YOU READ IT.
+echo.
+echo.
+echo.
+echo                                           __________________________________
+echo                                          /                                  \
+echo                                          \         ALL SURVIVING            /
+echo                                          /      G25's HAVE GOTTEN A DEADLY  \
+echo                                          \              VIRUS!              /
+echo                                          /                                  \
+echo                                          \                                  /
+echo                                          /                                  \
+echo                                          \__________________________________/
+echo.
+echo.
+echo.
+pause
+cls
+echo.
+echo.
+echo.
+echo                                So What?
+echo                                 __ __                          _____
+echo                                /'\ /'\                        /*   *\
+echo                               /  ___  \                       \/\/\/\
+echo                              / \_____/ \                      \_____/
+echo                             /  /     \  \
+echo                              _/       \_
+pause
+cls
+echo.
+echo.
+echo.
+echo                           SO WHAT? THERE ARE SURVIVING MACHINES THAT CAN KILL YOU AND ME!!                              
+echo                                 __ __                          _____
+echo                                /'\ /'\                        /* _ *\
+echo                               /  ___  \                         (_)
+echo                              / \_____/ \                      \_____/
+echo                             /  /     \  \
+echo                              _/       \_
+pause
+cls
+echo.
+echo.
+echo.
+echo                                    What can we do about it?
+echo                                 __ __                            _\ /_
+echo                                /'\_/'\                          /*   *\
+echo                               /  (_)  \                         \/\/\/\
+echo                              / \_____/ \                        \_____/
+echo                             /  /     \  \
+echo                              _/       \_
+echo.
+echo.
+echo.
+pause
+cls
+echo.
+echo.
+echo.
+echo                                                       WE NEED TO FIND THE ANTIVIRUS
+echo                            __ __                              _____
+echo                           /'\ /'\                            /* _ *\
+echo                          /  ___  \                             (_)
+echo                         / \_____/ \                          \_____/
+echo                        /  /     \  \
+echo                         _/       \_
+echo.
+echo.
+echo.
+echo.
+pause
+cls
+echo.
+echo.
+echo.
+echo                       What's the antivirus?
+echo                            __ __                              _____
+echo                           /'\ /'\                            /*   *\
+echo                          /  ___  \                           \/\/\/\  
+echo                         / \_____/ \                          \_____/
+echo                        /  /     \  \
+echo                         _/       \_
+echo.
+echo.
+echo.
+echo.
+pause
+cls
+echo.
+echo.
+echo.
+echo                                                      LET ME TELL YOU...
+echo                            __ __                              _____
+echo                           /'\ /'\                            /* _ *\
+echo                          /  ___  \                             (_) 
+echo                         / \_____/ \                          \_____/
+echo                        /  /     \  \
+echo                         _/       \_
+echo.
+echo.
+echo.
+echo.
+pause
+cls
+echo.
+echo.
+echo.
+echo                     WE HAD MADE A ANTIVIRUS A LONG, LOOOOOOONG, LOOOOOOOOOOOOOONG TIME AGO.
+echo                     WE HAD MADE IT INCASE A VIRUS GOT OUT. IT CONNECTS TO THE SCOURCE AND 
+echo                     CONNECTS TO A NON INFESTED ROBOT THEN THE ROBOT GETS DESTROYED. WE NEED
+echo                     TO FIND A NON INFECTED ROBOT, THE ANTIVIRUS, AND TO FIND THE SCOURCE.
+echo.
+echo.
+echo.
+echo.
+pause
+cls
+echo.
+echo.
+echo.
+echo                                        ILL BRING YOU TO AN AREA THE COMPUTER CALLS THE VIRUS ZONE
+echo                            __ __                              _____
+echo                           /'\ /'\                            /* _ *\
+echo                          /  ___  \                             (_) 
+echo                         / \_____/ \                          \_____/
+echo                        /  /     \  \
+echo                         _/       \_
+echo.
+echo.
+echo.
+pause
+goto mapRPGstart
+
+:mapRPGstart
+cls
+echo.
+echo.
+echo.
+echo          /()\   (1)  
+echo                     \ \
+echo                      \ \________
+echo                       \________ \
+echo                                \ \
+echo                              LOCKED
+echo                         _________________
+echo                         \  Fried C      /
+echo                         /      Sh ck    \
+echo                         \________/\_____/
+echo                                       \ \_________
+echo                                        \___________LOCKED      I CAN'T SEE AHEAD. TYPE NUMBER AND PRESS ENTER 
+set /p maparea1=
+
+if %maparea1% == 1 goto begginningofRPG1
+goto mapRPGstart
+
+:begginningofRPG1
+cls
+echo.
+echo.
+echo.
+echo                                            WELCOME TO THE ROAD
+echo.
+echo                               ___
+echo                              /' '\
+echo                             /\___/\
+echo                              /   \                                         Robo thing
+echo                                                                    (1)   __
+echo                                                                         /()\
+echo                    (2) materials
+echo                                                     (3) whats that? (do last!)
+set /p bortype1=
+
+if bortype1 == 1 goto fightrobothing
+if bortype2 == 2 goto materialsgather1
+
+:fightrobothing
+set health=100
+set robohealth=50
+set playerdmg=7
+set robodmg=4
+goto battletrain
+
+:battletrain
+cls
+echo                                        
+echo                                              __ __                     ___
+echo You: %health%                                    /'\ /'\                   /(')\           
+echo Robot: %robohealth%                                   \____/ \____/             \___/
+echo                                             \_____/                    
+echo                                             /     \                    
+echo 1) Fight!                                  _/       \_                  \_/     
+echo                              
+set /p ForL=                                       
+
+if %ForL% == 1 goto attack1
+goto battletrain
+
+:attack1
+cls
+set /a health-=%dummydmg%
+set /a dummyhealth-=%playerdmg%
+if %robohealth% lss 0 goto mapRPGstart1
+if %health% lss 0 goto fightrobothing
+goto battletrain
+
+:mapRPGstart1
+cls
+echo.
+echo.
+echo.
+echo                                            WELCOME TO THE ROAD
+echo.
+echo                               ___
+echo                              /' '\
+echo                             /\___/\
+echo                              /   \                                         Robo thing
+echo                                                                    (x)   __
+echo                                                                         /()\
+echo                    (2) materials
+echo                                                     (3) whats that? (do last!)
+set /p bortype1=
+
+if bortype2 == 2 goto materialsgather2
+
+
+:materialsgather1
+
+goto mapRPGstart72
+:materialsgather2
+
+goto map3
+
+
+:mapRPGstart72
+cls
+echo.
+echo.
+echo.
+echo                                            WELCOME TO THE ROAD
+echo.
+echo                               ___
+echo                              /' '\
+echo                             /\___/\
+echo                              /   \                                         Robo thing
+echo                                                                    (1)   __
+echo                                                                         /()\
+echo                    (x) materials
+echo                    
+echo
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
